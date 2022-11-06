5695f5705aa970cd37d08387f3d3d55a441d8a15
Ayange Felix <ayangefelix8@gmail.com>
2022-11-06 21:53:22 +0100

    add product serializer validators

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 from orders.models import Product
-
+from rest_framework.validators import UniqueValidator
 
 class ProductSerializer(serializers.ModelSerializer):
+    PIZZA_SIZES=(
+        ('SMALL','Small'),
+        ('MEDIUM','Medium'),
+        ('LARGE','Large'),
+        ('EXTRA_LARGE','Extra_Large'),
+    )
+   
+    price = serializers.DecimalField(max_digits=20, decimal_places=2)
+    flavour = serializers.CharField(max_length=40)
+    size = serializers.ChoiceField(choices=PIZZA_SIZES)
+    stock = serializers.IntegerField()
     image_url = serializers.ReadOnlyField()
     class Meta:
         model = Product
@@ -15,5 +26,9 @@ class ProductSerializer(serializers.ModelSerializer):
         return representation
     
     def validate(self, attrs):
-        
+        product_exists = Product.objects.filter(flavour=attrs['flavour']).exists()
+        if product_exists:
+            raise serializers.ValidationError(detail=f"Product with the name {attrs['flavour']} already exists")
         return super().validate(attrs)
+    
+    
