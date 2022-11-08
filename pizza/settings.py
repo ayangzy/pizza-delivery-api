0923b803efc2606287868f7ad97f9fc75a727f4d
Ayange Felix <ayangefelix8@gmail.com>
2022-11-08 16:23:29 +0100

    add mail keys

--- pizza/settings.py
+++ pizza/settings.py
@@ -174,17 +174,18 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # email config
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT =  587
-# EMAIL_HOST_USER = config('EMAIL_HOST')
-# EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
-# EMAIL_USE_TLS = True
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = '2525'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT =  587
+EMAIL_HOST_USER = config('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# EMAIL_HOST = 'smtp.mailtrap.io'
+# EMAIL_HOST_USER = config('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
+# EMAIL_PORT = '2525'
 
 
 DJOSER = {
