3af02eca7b3ac11df3de744b9e47c509e1610c6b
Ayange Felix <ayangefelix8@gmail.com>
2022-11-09 10:19:55 +0100

    update db settings

--- pizza/settings.py
+++ pizza/settings.py
@@ -117,15 +117,25 @@ WSGI_APPLICATION = "pizza.wsgi.application"
 
 DATABASES={}
 
-if DEBUG:
-    DATABASES["default"]={
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+# if DEBUG:
+#     DATABASES["default"]={
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
 
+#     }
+# else:
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-    DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+}
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 #db_from_env = dj_database_url.config(conn_max_age=600)
 #DATABASES['default'].update(db_from_env)
